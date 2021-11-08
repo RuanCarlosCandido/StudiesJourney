265816605d3efa5b37533bc7d3bf4f695f54b8fd
ruancarloscandido <ruan.candido@hotmail.com>
2021-11-07 22:30:07 -0300

    morestreams

--- src/org/general/Lists/Exer.java
+++ src/org/general/Lists/Exer.java
@@ -19,6 +19,7 @@ public class Exer {
 		dogKennel.add(new Dog("German Sheperd", "Thor", 3));
 		dogKennel.add(new Dog("German Sheperd", "k9", 6));
 		dogKennel.add(new Dog("Dobermann", "Pest", 9));
+		dogKennel.add(new Dog("Poodle", "Mel", 9));	
 		dogKennel.add(null);
 
 		/* Given a list of Dogs, you need to find all the dogs whose age is greater than
@@ -53,19 +54,36 @@ public class Exer {
 		if(d1.isPresent())
 			System.out.println("using findAny "+d1.get());
 
-		
+
 		//Given a list of dogs, You need to find highest age of dogs?
 		OptionalInt max = dogKennel.stream().filter(dog -> dog!=null).mapToInt(Dog::getAge).max();
 		if(max.isPresent())
 			System.out.println("max= " + max.getAsInt());
-		
-		
+
+
 		//Given a list of Dogs, you need sort Dogs list by age?
 		dogKennel.stream()
 		.filter(dog -> dog != null)
 		.sorted((dog1,dog2)->dog1.getAge()-dog2.getAge())
 		.forEach(System.out::println);;
-		
+
+
+		//Given the list of Dogs, you need to join the all dogs names with ","?
+		String joinedNames = String.join(",",
+				dogKennel.stream()
+				.filter(dog -> dog != null)
+				.map(Dog::getName)
+				.collect(Collectors.toList()));
+		System.out.println("joined_names = " +joinedNames);
+
+
+		//Given the list of dogs, you need to group them by name
+				dogKennel.stream()
+				.filter(dog -> dog != null)
+				.collect(Collectors.groupingBy(Dog::getName))
+				.forEach((name, list) ->System.out.println("Name: "+name+"==>"+list));
+
+
 	}
 
 }
